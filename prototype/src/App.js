ddce1f57cea97d61aa700d27fd3d8ded2ca099e6
Chris Gough <cwgough@bu.edu>
2022-12-08 01:42:03 -0500

    fixed mongoDB authentication issue, added env.TEMPLATE

--- prototype/src/App.js
+++ prototype/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import "./App.css";
 import { useState, useEffect } from 'react';
 import SearchIcon from './search.svg'
