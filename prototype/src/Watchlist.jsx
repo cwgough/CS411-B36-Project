515683e1dacf5b434ecc7fb2f515014a1027efdd
grguerra01 <98914563+grguerra01@users.noreply.github.com>
2022-12-12 00:05:24 -0500

    Add files via upload

--- prototype/src/Watchlist.jsx
+++ prototype/src/Watchlist.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
 
-const Table = (movieRows) => {
+const Table = () => {
+    const [movieRows, setMovieRows] = useState([]);
+
+    async function getWatchlist() {
+        const response = await fetch(`http://localhost:8080/watchlist`, { mode: 'cors' })
+        const data = await response.json()
+        return data
+    }
+
+    async function loadTableData() {
+        const hold = await getWatchlist()
+            .then(res => setMovieRows(res))
+        // console.log(data)
+    }
 
     function renderTableData() {
-        return movieRows.renderList.map((movie, index) => {
+        // loadTableData()
+        return movieRows.map((movie, index) => {
             const { titleID, titleText, locationsFilmed, provider } = movie
             return (
                 <tr key={titleID}>
                     <td>{index}</td>
                     <td>{titleText}</td>
-                    {/* <td><Popup trigger={<button> Trigger</button>} position="right center">
-                    <div>{locationsFilmed}</div>
-                    </Popup></td> */}
-                    <td>{locationsFilmed}</td>
+                    <td><Popup trigger={<button>View Locations</button>} position="right center">
+                    <div><p>{locationsFilmed}</p></div>
+                    </Popup></td>
                     <td>{provider}</td>
                     <td><button type='button' className='deleteButton'>Remove</button></td>
                 </tr>
             )
         })
     }
+
+    loadTableData();
+
 
     return (
         <div>
